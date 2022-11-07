468731ac893b12b9bbe4671308f8427447e76b88
belgoros <s.cambour@gmail.com>
2022-11-07 16:53:20 +0100

    Implement update and destroy line item date

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -1,9 +1,15 @@
 class LineItemDatesController < ApplicationController
   before_action :set_quote
+  before_action :set_line_item_date, only: [:edit, :update, :destroy]
 
   def new
     @line_item_date = @quote.line_item_dates.build
   end
+
+  def edit
+
+  end
+
 
   def create
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
@@ -15,13 +21,31 @@ class LineItemDatesController < ApplicationController
     end
   end
 
+  def update
+    if @line_item_date.update(line_item_date_params)
+      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item_date.destroy
+
+    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+  end
+
   private
 
-  def line_item_date_params
-    params.require(:line_item_date).permit(:date)
-  end
+    def line_item_date_params
+      params.require(:line_item_date).permit(:date)
+    end
 
-  def set_quote
-    @quote = current_company.quotes.find(params[:quote_id])
-  end
+    def set_quote
+      @quote = current_company.quotes.find(params[:quote_id])
+    end
+
+    def set_line_item_date
+      @line_item_date = @quote.line_item_dates.find(params[:id])
+    end
 end
